9ddc9b7d28d2c7d8fa23165741fd5ea090180297
Milashkarr <astashenkova.mk@gmail.com>
2024-04-08 16:05:17 +0300

    [study] Change build.bat and add exe to ignore

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ setlocal EnableDelayedExpansion
 :: "Ninja" / "ninja"
 :: "Visual Studio 17 2022" / "visual_studio"
 ::
-set BUILD_TYPE=Ninja
-set BUILD_SUFFIX=ninja
+set BUILD_TYPE="Visual Studio 17 2022"
+set BUILD_SUFFIX=visual_studio
 
 chcp 65001
 
